a8e811d3fe61912a7b1f5fdb83273c69a0c1fc8c
Phones <paulohenriqueh178h@gmail.com>
2022-08-09 04:12:02 -0300

    Instalação dos programas por snap e flatpak adicionados. Codigo organizado

--- install-programs.sh
+++ install-programs.sh
@@ -7,6 +7,39 @@ RED='\033[0;31m'
 NC='\033[0m'
 # -------------------
 
+# ---------------------- Variaveis --------------------------
+CAMINHO_PASTA_DOWNLOADS_PROGRAMAS="$HOME/Downloads/Programas"
+
+LISTA_NOMES_PROGRAMAS_INSTALADOS=(
+    "git"
+    "python3"
+    "python-is-python3"
+    "python3-pip"
+    "snapd"
+    "spotify"
+    "google"
+    "vscode"
+    "discord"
+    "steam"
+    "OBS"
+)
+
+INSTALAR_POR_GERENCIADOR=(
+    git
+    wget
+    g++
+    python3
+    python3-pip
+    python-is-python3
+    nano
+    snapd
+    steam-installer
+    steam-devices
+    steam:i386
+)
+
+# ------------------------------------------------------------
+
 # ------------------  print with color -----------------------
 function pwc()
 {
@@ -29,17 +62,7 @@ function update_system()
 
 update_system
 
-# ------------------ Programas que podem ser instalados pelo gerenciador de pacotes ----------------------
-INSTALAR_POR_GERENCIADOR=(
-    git
-    wget
-    g++
-    python3
-    python3-pip
-    python-is-python3
-    nano
-    snapd
-)
+# ---------------- Instala programas que podem ser instalados pelo gerenciador de pacotes ------------------
 
 pwc "blue" "instalando programas via gerenciador de pacotes"
 for program_name in ${INSTALAR_POR_GERENCIADOR[@]}; do
@@ -57,20 +80,12 @@ pwc "green" "Instalação dos programas de gerenciador finalizado"
 
 # -------------- Faz a instalação dos programas que precisam ser baixados externamente ---------------------
 pwc "blue" "criando e acessando pasta de downloads dos programas externos"
-mkdir /home/paulohenrique/Downloads/Programas
-
-cd /home/paulohenrique/Downloads/Programas/
+mkdir $CAMINHO_PASTA_DOWNLOADS_PROGRAMAS
 
 DOWNLOAD_PROGRAMAS_EXTERNOS=(
     "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
     "https://az764295.vo.msecnd.net/stable/da76f93349a72022ca4670c1b84860304616aaa2/code_1.70.0-1659589288_amd64.deb"
     "https://dl.discordapp.net/apps/linux/0.0.18/discord-0.0.18.deb"
-)
-
-LISTA_NOMES_PROGRAMAS_EXTERNOS=(
-    "google"
-    "vscode"
-    "discord"
 )
 
 pwc "blue" "fazendo download de todos os programas externos"
@@ -81,13 +96,19 @@ done
 pwc "green" "todos os downloads foram finalizados"
 
 pwc "blue" "instalando todos os programas baixados"
-dpkg -i .deb
-
-pwc "green" "programas externos instalados"
-for program_name in ${LISTA_NOMES_PROGRAMAS_EXTERNOS[@]}; do
-    pwc "green" "   [✔] - $program_name"
-done
-
+dpkg -i $CAMINHO_PASTA_DOWNLOADS_PROGRAMAS/*.deb
 
 # ----------------------------------------------------------------------------------------------------------
 
+# ----------------- Instalação de programas via flatpack --------------
+flatpak install flathub com.obsproject.Studio -y
+# ---------------------------------------------------------------------
+
+# ----------------- Instalação dos pacotes por snap -------------------
+snap install spotify
+# ---------------------------------------------------------------------
+
+pwc "green" "programas instalados"
+for program_name in ${LISTA_NOMES_PROGRAMAS_EXTERNOS[@]}; do
+    pwc "green" "   [✔] - $program_name"
+done
